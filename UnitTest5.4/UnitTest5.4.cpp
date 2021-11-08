7edbe75ae6379c4482677c89c4b91d6980671dd9
olehhordii <oleh.hordii.ik.2021@lpnu.ua>
2021-11-08 23:35:47 +0200

    first commit master

---
+++ UnitTest5.4/UnitTest5.4.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.4/lab5.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest54
+{
+	TEST_CLASS(UnitTest54)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			int c = 5.57517;
+			t = S0(1, 6);
+			Assert::AreEqual(t, c);
+		}
+	};
+}
